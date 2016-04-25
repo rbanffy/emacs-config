3aea5dc32b0ffbf3272de7af73a174597595d806
Ricardo Banffy <rbanffy@gmail.com>
2016-04-25 18:44:26 +0100

    Automated configuration save

--- init.el
+++ init.el
@@ -42,7 +42,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("c4465c56ee0cac519dd6ab6249c7fd5bb2c7f7f78ba2875d28a50d3c20a59473" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default)))
+ '(custom-theme-load-path
+   (quote
+    ("/Users/ricardobanffy/.emacs.d/elpa/ample-zen-theme-20150119.1354/" "/Users/ricardobanffy/.emacs.d/elpa/zenburn-theme-20160416.1011/" custom-theme-directory t)))
+ '(exec-path
+   (quote
+    ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
