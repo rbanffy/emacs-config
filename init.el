0f8fa8ca83f3841f784070843a56d41c66f99931
Ricardo Banffy <rbanffy@gmail.com>
2016-04-25 11:34:58 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,32 @@
+; -*- coding:utf-8 -*-
+
+
+;; Add MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Avoid the startup screen
+(setq inhibit-startup-message t)
+
+;; Highlight the fill column
+(require 'fill-column-indicator)
+(fci-mode t)
+
+(ido-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "IBM 3270 Narrow"))))
+ '(linum ((t (:inherit default :foreground "#777" :background "#333" :box nil :height 100)))))
