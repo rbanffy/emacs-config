64e6e251525fde67cbcf17b881310ddd5dc73b07
Ricardo Banffy <rbanffy@gmail.com>
2016-05-27 14:26:17 -0700

    Prevent tabification

--- init.el
+++ init.el
@@ -85,6 +85,7 @@
  '(exec-path
    (quote
     ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
+ '(indent-tabs-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
