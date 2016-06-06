bd35626de538d57bbd0ce0bcde51e24e7f3d46d2
Ricardo Bánffy <rbanffy@gmail.com>
2016-06-06 22:54:23 +0100

    Remove some Mac-specific misbehavior

--- init.el
+++ init.el
@@ -97,17 +97,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(custom-safe-themes
    (quote
     ("c4465c56ee0cac519dd6ab6249c7fd5bb2c7f7f78ba2875d28a50d3c20a59473" "1db337246ebc9c083be0d728f8d20913a0f46edc0a00277746ba411c149d7fe5" default)))
  '(custom-theme-load-path
    (quote
-    ("/Users/ricardobanffy/.emacs.d/elpa/ample-zen-theme-20150119.1354/" "/Users/ricardobanffy/.emacs.d/elpa/zenburn-theme-20160416.1011/" custom-theme-directory t)))
+    ("~/.emacs.d/elpa/ample-zen-theme-20150119.1354/" "/Users/ricardobanffy/.emacs.d/elpa/zenburn-theme-20160416.1011/" custom-theme-directory t)) t)
  '(exec-path
    (quote
     ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
  '(indent-tabs-mode nil)
+ '(package-selected-packages
+   (quote
+    (zencoding-mode zenburn-theme yasnippet yaml-mode selectric-mode popup-complete markdown-mode+ flymake-jshint flycheck fill-column-indicator auto-complete)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
