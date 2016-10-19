16555e19ab437c31db88bf8f021016372243a364
Ricardo Banffy <rbanffy@gmail.com>
2016-10-19 22:12:29 +0100

    Custom did it

--- init.el
+++ init.el
@@ -149,8 +149,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "IBM 3270 Narrow"))))
- '(linum ((t (:inherit default :foreground "#777" :background "#333" :box nil :height 100)))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box
+ nil :strike-through nil :overline nil :underline nil :slant
+ normal :weight normal :height 120 :width normal
+ :family "IBM 3270 Narrow"))))
+ '(linum ((t (:inherit default :foreground "#777" :background "#333" :box nil :height 100))))
+ '(minimap-active-region-background ((t (:background "gray37")))))
 
 (provide 'init)
 ;;; init.el ends here
