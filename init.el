6831235f7eac64bb15b7737691f4e1a5024977a6
Ricardo Bánffy <rbanffy@gmail.com>
2018-07-21 16:02:50 -0400

    Fix issue with 3270 font being unloadable on the newest Emacs

--- init.el
+++ init.el
@@ -26,19 +26,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-enabled-themes nil)
  '(exec-path
    (quote
     ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "~/.pyenv/shims/" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_9" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/usr/local/bin")))
  '(indent-tabs-mode nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (ac-html auto-complete dockerfile-mode electric-spacing elfeed elfeed-goodies fill-column-indicator flycheck flymake-jshint lua-mode markdown-mode+ popup-complete selectric-mode web-completion-data web-mode yaml-mode yasnippet zencoding-mode)))
  '(scroll-bar-mode nil)
+ '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :family "IBM 3270 Narrow")))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
 
 (provide 'init)
 ;;; init.el ends here
